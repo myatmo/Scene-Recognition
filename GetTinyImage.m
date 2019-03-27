25fa7cbe5c474a675d0cfc8d2de45b684f1b58cc
Myat Mo <myatmo@outlook.com>
2019-03-27 02:22:44 -0500

    Add files via upload

---
+++ GetTinyImage.m
@@ -0,0 +1,22 @@
+function [feature] = GetTinyImage(I, output_size)
+%Input: I is an gray scale image, output_size=[w,h] is the size of the tiny image.
+%Output: feature is the tiny image representation by vectorizing the pixel intensity in
+%a column major order. The resulting size will be w×h.
+%Description: You will simply resize each image to a small, fixed resolution (e.g.,
+%16×16). You need to normalize the image by having zero mean and unit length. This
+%is not a particularly good representation, because it discards all of the high frequency
+%image content and is not especially invariant to spatial or brightness shifts.
+
+w = output_size(1);
+h = output_size(2);
+
+% resizing the result vector
+J = imresize(I, [w,h]);
+J = im2double(J);
+J = (J-mean(J(:))) / std(J(:));
+
+% vectorization of the image in column-major order
+feature = reshape(J,[],1);
+feature = feature';
+
+
