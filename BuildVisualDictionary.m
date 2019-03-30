a579871981d6b3168a05244633fd163c813057d1
Myat Mo <myatmo@outlook.com>
2019-03-29 23:02:48 -0500

    Add files via upload

--- BuildVisualDictionary.m
+++ BuildVisualDictionary.m
@@ -7,23 +7,37 @@ function [vocab] = BuildVisualDictionary(training_image_cell, dic_size)
 %built-in functions: vl_dsift from VLFeat, kmeans from MATLAB toolbox.
 
 % Visual Dictionary Building Algorithm
-binSize = 6;
-des_list = [];
-for i = 1:length(training_image_cell)
+binSize = 4;
+step = 10;
+N_images = length(training_image_cell);
+disp(N_images);
+N_each = ceil(10000/N_images);
+disp(N_each);
+descriptors = zeros(128, N_images * N_each);
+count = 0;
+for i = 1:N_images
     %1: For each image, compute dense SIFT over regular grid
-    I = training_image_cell{i};
     %Is = imgaussfilt(I, 4); % gaussian smoothing
-    I = im2single(I); % convert to single
-    [f, d] = vl_dsift(I, 'size', binSize); % f: 2 x num_of_keypoints, d: 128 x num_of_keypoints
+    I = im2single(training_image_cell{i}); % convert to single
+    [~, features] = vl_dsift(I, 'Step', step); % f: 2 x num_of_keypoints, d: 128 x num_of_keypoints
     
+    %keyboard
+    whos features
+    %features = features';
+    count = count + 1
     %2: Build a pool of SIFT features from all training images
-    des_list = [des_list; d'];
+    descriptors(:,N_each * (i-1) + 1 : N_each * i) = features(:,1:N_each);
+    
+    %descriptors(:,N_each * (i-1) + 1 : N_each * i) = features(:,1:N_each);
 end
-
-des_list = im2double(des_list); % convert to double
 
 %3: Find cluster centers from the SIFT pool using kmeans algorithms and
 %   return the cluster centers as vocab.
-[idx, vocab] = kmeans(des_list, dic_size);
+descriptors = single(descriptors');
+%keyboard
+gpuDesc = gpuArray(descriptors);
+[~,centers]=kmeans(gpuDesc,dic_size,'MaxIter',10000);
+%[~, centers] = kmeans(descriptors, dic_size);
+vocab = single(centers);
 
 
